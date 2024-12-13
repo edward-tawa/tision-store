acb7f858ce2b39a17058360be9b2196712711125
taguma tawa <tagumtawa@gmail.com>
2024-12-13 13:21:00 +0200

    changes in settings

--- store/settings.py
+++ store/settings.py
@@ -206,7 +206,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = '/app/staticfiles'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
