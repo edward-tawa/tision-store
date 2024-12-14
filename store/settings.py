3ba64dc45a8bededa9eae5047204702d262f050a
taguma tawa <tagumtawa@gmail.com>
2024-12-14 11:02:34 +0200

    settings change

--- store/settings.py
+++ store/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME'), "localhost"]
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 
 # Application definition
